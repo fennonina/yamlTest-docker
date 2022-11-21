e1899de0ae1f460e88746efdd3c13832dd2c30de
fennonina <118367405+fennonina@users.noreply.github.com>
2022-11-21 02:30:34 -0800

    Update config.sh

--- config.sh
+++ config.sh
@@ -3,4 +3,3 @@ echo -----[ starting ]------
 echo -----------------------
 docker pull shellngn/pro:latest
 docker run --mount source=shellngn-data,target=/home/node/server/data -p 8080:8080 -e HOST=0.0.0.0 shellngn/pro:latest
-bash url.sh
